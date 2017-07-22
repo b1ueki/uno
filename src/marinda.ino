41b33bbf36eff729566765031c9e83ebf6dfa163
岩田和俊 <k4z@iwatakazutoshinoMacBook-Pro.local>
2017-07-22 20:24:48 +0900

    program orthopaedy

--- src/marinda.ino
+++ src/marinda.ino
@@ -5,11 +5,12 @@
 #define SOLENOID 2
 
 Servo servo1, servo2;
-int time = 0;
-char key, M_m, *buf, temp = 0;
+int counter = 0, time;//カウンタ変数,ディレイ時間
+int mag;//倍率
+char key, M_m;//キー,major or minor
 int D_M = 0, E_M = 0, F_M = 0, G_M = 0, A_M = 0, B_M = 0;
-int a, b;
-int note, scale, oct;
+int note, scale, oct;//長さ,高さ,オクターブ
+int angle1, angle2;
 
 void setup(){
   Serial.begin(9600);
@@ -19,237 +20,156 @@ void setup(){
 }
 
 void loop(){
-  time++;
+  if(Serial.available() > -1){
+    if(counter++ == 0){
+      //    key = Serial.read();
+      //    M_m = Serial.read();
+      //
+      //    if(key == 'D' && M_m == 'M' || key == 'B' && M_m == 'm'){
+      //      D_M++;
+      //    }
+      //    else if(key == 'E' && M_m == 'M' || key == 'c' && M_m == 'm'){
+      //      E_M++;
+      //    }
+      //    else if(key == 'F' && M_m == 'M' || key == 'D' && M_m == 'm'){
+      //      F_M++;
+      //    }
+      //    else if(key == 'G' && M_m == 'M' || key == 'E' && M_m == 'm'){
+      //      G_M++;
+      //    }
+      //    else if(key == 'A' && M_m == 'M' || key == 'f' && M_m == 'm'){
+      //      A_M++;
+      //    }
+      //    else if(key == 'B' && M_m == 'M' || key == 'g' && M_m == 'm'){
+      //      B_M++;
+      //    }
+    }
+    else{
+      note = Serial.read();
+      scale = Serial.read();
+      oct = Serial.read();
 
-  //if(Serial.available() > 0){
-  //    if(time == 1){
-  //      a = Serial.read();
-  //      b = Serial.read();
-  //    }
-  //    else if(time == 2){
-  //      key = Serial.read();
-  //      M_m = Serial.read();
-  //
-  //      if(key == 'D' && M_m == 'M' || key == 'B' && M_m == 'm'){
-  //        D_M++;
-  //      }
-  //      else if(key == 'E' && M_m == 'M' || key == 'c' && M_m == 'm'){
-  //        E_M++;
-  //      }
-  //      else if(key == 'F' && M_m == 'M' || key == 'D' && M_m == 'm'){
-  //        F_M++;
-  //      }
-  //      else if(key == 'G' && M_m == 'M' || key == 'E' && M_m == 'm'){
-  //        G_M++;
-  //      }
-  //      else if(key == 'A' && M_m == 'M' || key == 'f' && M_m == 'm'){
-  //        A_M++;
-  //      }
-  //      else if(key == 'B' && M_m == 'M' || key == 'g' && M_m == 'm'){
-  //        B_M++;
-  //      }
-  //    }
-  //    else{
-  if(Serial.available() > 50)
-    Serial.write(0);
-  note = Serial.read();
-  scale = Serial.read();
-  oct = Serial.read();
-  Serial.println (note);
-  Serial.println (scale);
-  Serial.println (oct);
-  //      note = '4';
-  //      scale = 'G';
-  //      oct = '4';
+      if(D_M == 1){
+        if(scale == 'F')
+          scale = 'f';
+        else if (scale == 'C')
+          scale = 'c';
+      }
+      else if(E_M == 1){
+        if(scale == 'F')
+          scale = 'f';
+        else if(scale == 'G')
+          scale = 'g';
+        else if(scale == 'C')
+          scale = 'c';
+        else if(scale == 'D')
+          scale = 'd';
+      }
+      else if(F_M == 1){
+        if(scale == 'B')
+          scale = 'a';
+      }
+      else if(G_M == 1){
+        if(scale == 'F')
+          scale = 'f';
+      }
+      else if(A_M == 1){
+        if(scale == 'C')
+          scale = 'c';
+        else if(scale == 'F')
+          scale = 'f';
+        else if(scale == 'G')
+          scale = 'g';
+      }
+      else if(B_M == 1){
+        if(scale == 'C')
+          scale = 'c';
+        else if(scale == 'D')
+          scale = 'd';
+        else if(scale == 'F')
+          scale = 'f';
+        else if(scale == 'G')
+          scale = 'g';
+        else if(scale == 'A')
+          scale = 'a';
+      }
 
-  //  if(D_M == 1){
-  //    if(scale == 'F')
-  //      scale = 'f';
-  //    else if (scale == 'C')
-  //      scale = 'c';
-  //  }
-  //  else if(E_M == 1){
-  //    if(scale == 'F')
-  //      scale = 'f';
-  //    else if(scale == 'G')
-  //      scale = 'g';
-  //    else if(scale == 'C')
-  //      scale = 'c';
-  //    else if(scale == 'D')
-  //      scale = 'd';
-  //  }
-  //  else if(F_M == 1){
-  //    if(scale == 'B')
-  //      scale = 'a';
-  //  }
-  //  else if(G_M == 1){
-  //    if(scale == 'F')
-  //      scale = 'f';
-  //  }
-  //  else if(A_M == 1){
-  //    if(scale == 'C')
-  //      scale = 'c';
-  //    else if(scale == 'F')
-  //      scale = 'f';
-  //    else if(scale == 'G')
-  //      scale = 'g';
-  //  }
-  //  else if(B_M == 1){
-  //    if(scale == 'C')
-  //      scale = 'c';
-  //    else if(scale == 'D')
-  //      scale = 'd';
-  //    else if(scale == 'F')
-  //      scale = 'f';
-  //    else if(scale == 'G')
-  //      scale = 'g';
-  //    else if(scale == 'A')
-  //      scale = 'a';
-  //  }
+      //角度を決めるための倍率を決める
+      switch(scale){
+        case 'C':
+          mag = 1;
+          break;
+        case 'c':
+          mag = 2;
+          break;
+        case 'D':
+          mag = 3;
+          break;
+        case 'd':
+          mag = 4;
+          break;
+        case 'E':
+          mag = 5;
+          break;
+        case 'e':
+          mag = 6;
+          break;
+        case 'F':
+          mag = 7;
+          break;
+        case 'f':
+          mag = 8;
+          break;
+        case 'G':
+          mag = 9;
+          break;
+        case 'g':
+          mag = 10;
+          break;
+        case 'A':
+          mag = 11;
+          break;
+        case 'a':
+          mag = 12;
+          break;
+        case 'B':
+          mag = 13;
+          break;
+        case 'b':
+          mag = 14;
+          break;
+      }
 
-  if(scale == 'C'){ //C
-    if(oct == '4'){
-      servo1.write(172.5);
-    }
-    else if(oct == '5'){
-      servo2.write(90);
-    }
-    digitalWrite(SOLENOID, HIGH);
-    delay(250);
-    digitalWrite(SOLENOID, LOW);
-  }
-  else if(scale == 'c'){ //C#
-    if(oct == '4'){
-      servo1.write(165);
-    }
-    else if(oct == '5'){
-      servo2.write(82.5);
-    }
-    digitalWrite(SOLENOID, HIGH);
-    delay(250);
-    digitalWrite(SOLENOID, LOW);
-  }
-  else if(scale == 'D'){ //D
-    if(oct == '4'){
-      servo1.write(157.5);
-    }
-    else if(oct == '5'){
-      servo2.write(75);
-    }
-    digitalWrite(SOLENOID, HIGH);
-    delay(250);
-    digitalWrite(SOLENOID, LOW);
-  }
-  else if(scale == 'd'){ //D#
-    if(oct == '4'){
-      servo1.write(150);
-    }
-    else if(oct == '5'){
-      servo2.write(67.5);
-    }
-    digitalWrite(SOLENOID, HIGH);
-    delay(250);
-    digitalWrite(SOLENOID, LOW);
-  }
-  else if(scale == 'E'){ //E
-    if(oct == '4'){
-      servo1.write(142.5);
-    }
-    else if(oct == '5'){
-      servo2.write(60);
-    }
-    digitalWrite(SOLENOID, HIGH);
-    delay(250);
-    digitalWrite(SOLENOID, LOW);
-  }
-  else if(scale == 'F'){ //F
-    if(oct == '4'){
-      servo1.write(135);
-    }
-    else if(oct == '5'){
-      servo2.write(52.5);
-    }
-    digitalWrite(SOLENOID, HIGH);
-    delay(250);
-    digitalWrite(SOLENOID, LOW);
-  }
-  else if(scale == 'f'){ //F#
-    if(oct == '4'){
-      servo1.write(127.5);
-    }
-    else if(oct == '5'){
-      servo2.write(45);
-    }
-    digitalWrite(SOLENOID, HIGH);
-    delay(250);
-    digitalWrite(SOLENOID, LOW);
-  }
-  else if(scale == 'G'){ //G
-    if(oct == '4'){
-      servo1.write(120);
-    }
-    else if(oct == '5'){
-      servo2.write(37.5);
-    }
-    digitalWrite(SOLENOID, HIGH);
-    delay(250);
-    digitalWrite(SOLENOID, LOW);
-  }
-  else if(scale == 'g'){ //G#
-    if(oct == '4'){
-      servo1.write(112.5);
-    }
-    else if(oct == '5'){
-      servo2.write(30);
-    }
-    digitalWrite(SOLENOID, HIGH);
-    delay(250);
-    digitalWrite(SOLENOID, LOW);
-  }
-  else if(scale == 'A'){ //A
-    if(oct == '4'){
-      servo1.write(105);
-    }
-    else if(oct == '5'){
-      servo2.write(22.5);
-    }
-    digitalWrite(SOLENOID, HIGH);
-    delay(250);
-    digitalWrite(SOLENOID, LOW);
-  }
-  else if(scale == 'a'){ //A#
-    if(oct == '4'){
-      servo1.write(97.5);
-    }
-    else if(oct == '5'){
-      servo2.write(15);
-    }
-    digitalWrite(SOLENOID, HIGH);
-    delay(250);
-    digitalWrite(SOLENOID, LOW);
-  }
-  else if(scale == 'B'){ //B
-    if(oct == '4'){
-      servo1.write(90);
-    }
-    else if(oct == '5'){
-      servo2.write(7.5);
-    }
-    digitalWrite(SOLENOID, HIGH);
-    delay(250);
-    digitalWrite(SOLENOID, LOW);
-  }
+      //角度を決める
+      angle1 = 180 - 7.5 * mag;
+      angle2 = angle1 - 82.5;
+      //サーボを動かす
+      if(oct == '4'){
+        servo1.write(angle1);
+      }
+      else if(oct == '5'){
+        servo2.write(angle2);
+      }
 
-  if(note == '4'){
-    delay(250);
+      //ディレイ時間を決める
+      switch(note){
+        case '4':
+          time = 250;
+          break;
+        case '2':
+          time = 500;
+          break;
+        case '1':
+          time = 750;
+          break;
+      }
+      delay(time);//ディレイする
+    }
   }
-  else if(note == '2'){
-    delay(500);
+  else{
+    servo1.write(90);
+    servo2.write(90);
+    delay(1000);
   }
-  else if(note == '1'){
-    delay(750);
-  }
-  //    }
-  //  }
 }
 
